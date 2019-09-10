222630f9e8e0336fafae8452436ea52d5d5604c4
emachiels <e.machiels@hotmail.com>
2019-09-10 20:50:02 +0200

    Implemented newable interface

--- src/body/contracts/application/ApplicationContract.ts
+++ src/body/contracts/application/ApplicationContract.ts
@@ -1,13 +1,17 @@
-import {interfaces} from 'inversify';
-import {ProviderContract} from '../providers/ProviderContract';
+import { interfaces } from 'inversify';
+import { ProviderContract } from '../providers/ProviderContract';
+import { Newable } from '../../heart/support/interfaces/Newable';
 export interface ApplicationContract {
     isBooted(): boolean;
 
     bind<T>(
         provider: interfaces.ServiceIdentifier<T>,
-        constructor: {
-            new (...args: any[]): T;
-        }
+        constructor: Newable<T>
+    ): ApplicationContract;
+
+    bindFactory<T>(
+        provider: interfaces.ServiceIdentifier<T>,
+        factory: (context: interfaces.Context) => T
     ): ApplicationContract;
 
     instance<T>(
@@ -17,9 +21,7 @@ export interface ApplicationContract {
 
     singleton<T>(
         provider: interfaces.ServiceIdentifier<T>,
-        constructor: {
-            new (...args: any[]): T;
-        }
+        constructor: Newable<T>
     ): ApplicationContract;
 
     get<T>(provider: interfaces.ServiceIdentifier<T>): T;
