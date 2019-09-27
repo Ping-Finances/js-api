72e2c22dfa66deed963f15889c7ccd6e3c2cb089
emachiels <e.machiels@hotmail.com>
2019-09-27 19:16:29 +0200

    Removed unnecessary on booted listener

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,3 @@
 import { Application } from './body/app/Application';
-import * as winston from 'winston';
 
 const application = new Application();
-
-application.onBooted(() => {
-    application.get<winston.Logger>('log');
-});
