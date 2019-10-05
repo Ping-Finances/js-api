00da4f45b3d4d0aa127460d41e509b3e4dbd7cb8
emachiels <e.machiels@hotmail.com>
2019-10-05 23:05:54 +0200

    Added documentation, added path getters,

--- src/body/app/Application.ts
+++ src/body/app/Application.ts
@@ -7,17 +7,16 @@ import { LoggingServiceProvider } from '../logging/LoggingServiceProvider';
 import { Newable } from '../support/interfaces/Newable';
 import { FilesystemServiceProvider } from '../filesystem/FilesystemServiceProvider';
 import { NotBoundError } from './NotBoundError';
-import { ConfigContract } from '../contracts/config/ConfigContract';
-import { Repository } from '../config/Repository';
+import { ConfigServiceProvider } from '../config/ConfigServiceProvider';
 
 export class Application extends EventsEmitter implements ApplicationContract {
     private booted = false;
 
     private static container: Container;
 
-    private rootPath: string | undefined;
+    private rootPath: string | null;
 
-    public constructor(root?: string) {
+    public constructor(root: string | null = null) {
         super();
 
         this.emit('boot:before');
@@ -31,11 +30,17 @@ export class Application extends EventsEmitter implements ApplicationContract {
         });
     }
 
-    private initialize(): Promise<void[]> {
+    /**
+     * Initializes the application
+     *
+     * @since 1.0.0
+     */
+    public initialize(): Promise<void[]> {
         return Promise.all([
             this.registerBaseBindings(),
             this.registerProvider(new LoggingServiceProvider(this)),
-            this.registerProvider(new FilesystemServiceProvider(this))
+            this.registerProvider(new FilesystemServiceProvider(this)),
+            this.registerProvider(new ConfigServiceProvider(this))
         ]);
     }
 
@@ -43,13 +48,10 @@ export class Application extends EventsEmitter implements ApplicationContract {
      * Registers basic services in the container.
      * These bindings are needed when booting the application.
      *
-     * @return {void}
-     *
      * @since 1.0.0
      */
     private async registerBaseBindings(): Promise<void> {
-        this.instance<ApplicationContract>('app', this);
-        this.singleton<ConfigContract>('config', Repository);
+        await this.instance<ApplicationContract>('app', this);
     }
 
     /**
@@ -58,10 +60,6 @@ export class Application extends EventsEmitter implements ApplicationContract {
      * @since 1.0.0
      */
     public async registerProvider(provider: ProviderContract): Promise<void> {
-        if (provider.boot && typeof provider.boot === 'function') {
-            await provider.boot();
-        }
-
         if (provider.register && typeof provider.register === 'function') {
             await provider.register();
         }
@@ -94,17 +92,26 @@ export class Application extends EventsEmitter implements ApplicationContract {
         return this;
     }
 
+    /**
+     * Binds a class factory to the container
+     *
+     * @since 1.0.0
+     */
     public async bindFactory<T>(
         provider: interfaces.ServiceIdentifier<T>,
         factory: (context: interfaces.Context) => T
     ): Promise<ApplicationContract> {
-        const identifier = Application.convertToSymbolIfString(
-            provider
-        );
+        const identifier = Application.convertToSymbolIfString(provider);
 
-        this.getContainer()
-            .bind<T>(identifier)
-            .toDynamicValue(factory);
+        if (this.getContainer().isBound(identifier)) {
+            this.getContainer()
+                .rebind<T>(identifier)
+                .toDynamicValue(factory);
+        } else {
+            this.getContainer()
+                .bind<T>(identifier)
+                .toDynamicValue(factory);
+        }
 
         return this;
     }
@@ -119,9 +126,7 @@ export class Application extends EventsEmitter implements ApplicationContract {
         provider: interfaces.ServiceIdentifier<T>,
         instance: T
     ): Promise<ApplicationContract> {
-        const identifier = Application.convertToSymbolIfString(
-            provider
-        );
+        const identifier = Application.convertToSymbolIfString(provider);
 
         if (this.getContainer().isBound(identifier)) {
             this.getContainer()
@@ -136,13 +141,16 @@ export class Application extends EventsEmitter implements ApplicationContract {
         return this;
     }
 
+    /**
+     * Binds a singleton instance to the container
+     *
+     * @since 1.0.0
+     */
     public async singleton<T>(
         provider: interfaces.ServiceIdentifier<T>,
         constructor: Newable<T>
     ): Promise<ApplicationContract> {
-        const identifier = Application.convertToSymbolIfString(
-            provider
-        );
+        const identifier = Application.convertToSymbolIfString(provider);
 
         if (this.getContainer().isBound(identifier)) {
             this.getContainer()
@@ -159,6 +167,11 @@ export class Application extends EventsEmitter implements ApplicationContract {
         return this;
     }
 
+    /**
+     * Gets a binding out of the container
+     *
+     * @since 1.0.0
+     */
     public get<T>(provider: interfaces.ServiceIdentifier<T>): T {
         const identifier = Application.convertToSymbolIfString(provider);
 
@@ -171,6 +184,12 @@ export class Application extends EventsEmitter implements ApplicationContract {
         }
     }
 
+    /**
+     * Converts a string to a symbol to get a
+     * unique binding aut of the container
+     *
+     * @since 1.0.0
+     */
     private static convertToSymbolIfString(provider: any): symbol {
         if (typeof provider === 'string') {
             return Symbol.for(provider);
@@ -179,23 +198,43 @@ export class Application extends EventsEmitter implements ApplicationContract {
         return provider;
     }
 
+    /**
+     * Gets the container
+     *
+     * @since 1.0.0
+     */
     public getContainer(): Container {
         return Application.container;
     }
 
-    public isBooted(): boolean {
-        return this.booted;
-    }
-
-    public onBooted(callback: (...args: any[]) => void): void {
-        this.on('booted', callback);
-    }
-
+    /**
+     * Sets the root folder path of the application
+     *
+     * @since 1.0.0
+     */
     public setRoothPath(path: string): void {
         this.rootPath = path;
     }
 
-    public getRootPath(): string {
-        return this.rootPath || '';
+    /**
+     * Returns the root folder path of the application
+     *
+     * @since 1.0.0
+     */
+    public getRootPath(): string | null {
+        return this.rootPath;
+    }
+
+    /**
+     * Returns the head path
+     *
+     * @since 1.0.0
+     */
+    public getHeadPath(): string | null {
+        if (!this.getRootPath()) {
+            return null;
+        }
+
+        return `${this.getRootPath()}/head`;
     }
 }
--- src/body/contracts/application/ApplicationContract.ts
+++ src/body/contracts/application/ApplicationContract.ts
@@ -1,41 +1,97 @@
 import { interfaces } from 'inversify';
 import { ProviderContract } from '../providers/ProviderContract';
 import { Newable } from '../../support/interfaces/Newable';
+import Container = interfaces.Container;
 
 export interface ApplicationContract {
-    isBooted(): boolean;
-
+    /**
+     * Bind a service to the container, so it
+     * can be injected in services later on.
+     *
+     * @since 1.0.0
+     */
     bind<T>(
         provider: interfaces.ServiceIdentifier<T>,
         constructor: Newable<T>
     ): Promise<ApplicationContract>;
 
+    /**
+     * Binds a class factory to the container
+     *
+     * @since 1.0.0
+     */
     bindFactory<T>(
         provider: interfaces.ServiceIdentifier<T>,
         factory: (context: interfaces.Context) => T
     ): Promise<ApplicationContract>;
 
+    /**
+     * Register an instance to the container.
+     * When resolving, all returned objects are the same.
+     *
+     * @since 1.0.0
+     */
     instance<T>(
         provider: interfaces.ServiceIdentifier<T>,
         instance: T
     ): Promise<ApplicationContract>;
 
+    /**
+     * Binds a singleton instance to the container
+     *
+     * @since 1.0.0
+     */
     singleton<T>(
         provider: interfaces.ServiceIdentifier<T>,
         constructor: Newable<T>
     ): Promise<ApplicationContract>;
 
+    /**
+     * Gets a binding out of the container
+     *
+     * @since 1.0.0
+     */
     get<T>(provider: interfaces.ServiceIdentifier<T>): T;
 
+    /**
+     * Register a service provider in the application.
+     *
+     * @since 1.0.0
+     */
     registerProvider(provider: ProviderContract): Promise<void>;
 
+    /**
+     * Emits an event on the application
+     *
+     * @since 1.0.0
+     */
     emit(event: string | symbol, ...args: any[]): boolean;
 
-    onBooted(callback: (...args: any[]) => void): void;
+    /**
+     * Gets the container
+     *
+     * @since 1.0.0
+     */
+    getContainer(): Container;
 
-    initialize(): Promise<void>;
-
+    /**
+     * Sets the root folder path of the application
+     *
+     * @since 1.0.0
+     */
     setRoothPath(path: string): void;
 
-    getRootPath(): string;
+    /**
+     * Returns the root folder path of the application
+     *
+     * @since 1.0.0
+     */
+    getRootPath(): string | null;
+
+    /**
+     * Returns the head path
+     *
+     * @since 1.0.0
+     */
+    getHeadPath(): string | null;
 }
