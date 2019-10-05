e0052332f280ec5eff347a5162b101da6cb78bd5
emachiels <e.machiels@hotmail.com>
2019-10-05 23:07:40 +0200

    Added config get with dot notation test

--- test/config/config.test.ts
+++ test/config/config.test.ts
@@ -1,23 +1,17 @@
-import { decorate, injectable } from 'inversify';
 import { Application } from '../../src/body/app/Application';
 import { ConfigContract } from '../../src/body/contracts/config/ConfigContract';
-import EventEmitter from 'events';
-
-Object.getPrototypeOf(EventEmitter.prototype).constructor = Object;
-decorate(injectable(), EventEmitter);
 
 describe('config get test', () => {
     test('config resolved by path', async () => {
         // arrange
         const application = new Application();
-        await application.initialize();
         const config = await application.get<ConfigContract>('config');
-        config.loadFromPath(`${__dirname}/testconfigmap/`);
 
         // act
+        config.loadFromPath(`${__dirname}/testconfigmap/`);
 
         // asssert
-        expect(config.getConfig()).toEqual({
+        expect(config.getItems()).toEqual({
             config: { foo: 'bar' },
             baz: {
                 bar: {
@@ -25,5 +19,7 @@ describe('config get test', () => {
                 }
             }
         });
+
+        expect(config.get('config.foo')).toEqual('bar');
     });
-})
+});
